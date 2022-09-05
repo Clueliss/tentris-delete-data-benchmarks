939fb3ceb9fb65f4867da81e1447a8a635182cb9
Liss Heidrich <Clueliss@users.noreply.github.com>
2022-09-05 17:00:41 +0200

    more robust hypertrie upload and changed suites to match changes in query generator

--- roles/hypertrie/files/build_hypertrie.sh
+++ roles/hypertrie/files/build_hypertrie.sh
@@ -11,17 +11,21 @@ release() {
 
     hypertrie_deploy_version=$(conan inspect . --raw version)
 
+    conan remote add -f hypertrie_tmp http://localhost:9300
+    conan user -r hypertrie_tmp -p hypertrie hypertrie 
     conan remove -f "hypertrie/$hypertrie_deploy_version@dice-group/stable" || true
     conan create . "hypertrie/$hypertrie_deploy_version@dice-group/stable" --build missing -e hypertrie_deploy_version="$hypertrie_deploy_version" --profile ${conan_profile}
-    conan upload "hypertrie/$hypertrie_deploy_version@dice-group/stable" --force --all -r local
+    conan upload "hypertrie/$hypertrie_deploy_version@dice-group/stable" --force --all -r hypertrie_tmp
+    conan remote remove hypertrie_tmp
 }
 
 
 conan_server_dir="$1"
 conan_server --server_dir "$conan_server_dir" &
+sleep 5 # wait for server to start
 
 cd hypertrie_code
 release
+sleep 5 # wait for server to finish processing
 
-sleep 5
 exit 0
